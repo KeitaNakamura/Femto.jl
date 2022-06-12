767401498db365743c19c873a3a720afe65cc48e
Keita Nakamura <keita.nakamura.1109@gmail.com>
2022-06-12 20:20:34 +0900

    Fix Tri6

--- test/element.jl
+++ test/element.jl
@@ -1,10 +1,11 @@
 @testset "Element" begin
     for T in (Float64, Float32)
+        TOL = sqrt(eps(T))
         for shape in (Quad4(), Tri6())
             element = Element{T}(ScalarField(), shape)
             for qp in Femto.num_quadpoints(element)
                 @test sum(element.N[qp]) ≈ 1
-                @test norm(sum(element.dNdx[qp])) < sqrt(eps(T))
+                @test norm(sum(element.dNdx[qp])) < TOL
             end
         end
     end
