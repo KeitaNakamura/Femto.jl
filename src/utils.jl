45dac4a142908de1fee4a4e59890f79ec174557a
Keita Nakamura <keita.nakamura.1109@gmail.com>
2022-06-23 09:57:50 +0900

    Remove map_tuple

--- src/utils.jl
+++ src/utils.jl
@@ -1,28 +1,3 @@
-promote_tuple_length() = -1
-promote_tuple_length(xs::Type{<: NTuple{N, Any}}...) where {N} = N
-# apply map calculations only for tuples
-# if one of the argunents is not tuple, treat it as scalar
-@generated function map_tuple(f, xs::Vararg{Any, N}) where {N}
-    L = promote_tuple_length([x for x in xs if x <: Tuple]...)
-    if L == -1 # no tuples
-        quote
-            @_inline_meta
-            @_propagate_inbounds_meta
-            f(xs...)
-        end
-    else
-        exps = map(1:L) do i
-            args = [xs[j] <: Tuple ? :(xs[$j][$i]) : :(xs[$j]) for j in 1:N]
-            :(f($(args...)))
-        end
-        quote
-            @_inline_meta
-            @_propagate_inbounds_meta
-            tuple($(exps...))
-        end
-    end
-end
-
 # zero_recursive
 zero_recursive(::Type{Array{T, N}}) where {T, N} = Array{T, N}(undef, nfill(0, Val(N)))
 @generated function zero_recursive(::Type{T}) where {T}
