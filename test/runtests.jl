cb496150f283a3a8bb816d64a506c20617dd3fb4
Keita Nakamura <keita.nakamura.1109@gmail.com>
2022-06-24 20:27:59 +0900

    Modify order of tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,9 @@ using InteractiveUtils # for subtypes
 using StaticArrays
 
 include("utils.jl")
+
 include("shapes.jl")
 include("elements.jl")
-include("grid.jl")
 include("integration.jl")
+
+include("grid.jl")
