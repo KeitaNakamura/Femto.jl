7e5084c187e93a8e058f401e85e10e912dd1153d
Keita Nakamura <keita.nakamura.1109@gmail.com>
2022-07-03 15:52:36 +0900

    Refactor `solve!`, remove `SolutionVector` (#8)
    * Refactor `solve!` function
    
    * Remove create_solutionvector

--- src/solve.jl
+++ src/solve.jl
@@ -1,46 +1,11 @@
-##################
-# SolutionVector #
-##################
-
-struct SolutionVector{T} <: AbstractVector{T}
-    data::Vector{T}
-    mask::BitVector
-end
-SolutionVector{T}(n::Int) where {T} = SolutionVector(zeros(T, n), falses(n))
-
-function create_solutionvector(::Type{T}, fieldtype::FieldType, grid::Grid) where {T}
-    n = num_dofs(fieldtype, grid)
-    SolutionVector{T}(n)
-end
-create_solutionvector(fieldtype::FieldType, grid::Grid{T}) where {T} = create_solutionvector(T, fieldtype, grid)
-
-Base.size(x::SolutionVector) = size(x.data)
-Base.getindex(x::SolutionVector, i::Int) = (@_propagate_inbounds_meta; x.data[i])
-function Base.setindex!(x::SolutionVector, v, i::Int)
-    @_propagate_inbounds_meta
-    x.data[i] = v
-    x.mask[i] = true
-    x
-end
-function fillzero!(x::SolutionVector)
-    fillzero!(x.data)
-    fillzero!(x.mask)
-    x
-end
-
-##########
-# solve! #
-##########
-
-function solve!(U::SolutionVector, K::AbstractMatrix, F::AbstractVector)
-    @assert length(U) == size(K, 1) == length(F)
-    mask = U.mask
-    for i in Iterators.filter(i -> mask[i], eachindex(mask))
+function solve!(U::AbstractVector, K::AbstractMatrix, F::AbstractVector, dirichlet::AbstractVector{Bool})
+    @assert length(U) == size(K, 1) == length(F) == length(dirichlet)
+    @assert size(K, 1) == size(K, 2)
+    for i in Iterators.filter(i -> dirichlet[i], eachindex(dirichlet))
         @inbounds F .-= U[i] * K[:, i]
     end
-    fdofs = findall(.!mask)
+    fdofs = findall(.!dirichlet)
     @inbounds U[fdofs] = K[fdofs, fdofs] \ F[fdofs]
     U
 end
-solve!(U::SolutionVector, K::SparseMatrixIJV, F::AbstractVector) = solve!(U, sparse(K), F)
-
+solve!(U::AbstractVector, K::SparseMatrixIJV, F::AbstractVector, dirichlet::AbstractVector{Bool}) = solve!(U, sparse(K), F, dirichlet)
