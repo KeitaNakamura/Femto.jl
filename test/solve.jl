322dd7acfd98f4754e9c1c3f48c4c068873e07fe
Keita Nakamura <keita.nakamura.1109@gmail.com>
2022-07-13 12:38:15 +0900

    Refactor `solve!` function (#20)
    * Refactor solve! function
    
    * Add tests

---
+++ test/solve.jl
@@ -0,0 +1,13 @@
+@testset "Solve" begin
+    Random.seed!(1234)
+    K = sprand(50, 50, 0.2)
+    F = rand(50)
+    dirichlet = rand(Bool, 50)
+    # SparseMatrixCSC
+    U1 = zeros(50)
+    solve!(U1, K, F, dirichlet)
+    # Matrix
+    U2 = zeros(50)
+    solve!(U2, Array(K), F, dirichlet)
+    @test U1 ≈ U2
+end
