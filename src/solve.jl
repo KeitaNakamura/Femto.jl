f01f3d7e1c197b58de81c07e4eca0fa89f6ffbbb
Keita Nakamura <keita.nakamura.1109@gmail.com>
2022-07-13 09:39:24 +0900

    Refactor solve! function

--- src/solve.jl
+++ src/solve.jl
@@ -1,9 +1,27 @@
-function solve!(U::AbstractVector, K::AbstractMatrix, F::AbstractVector, dirichlet::AbstractVector{Bool})
-    @assert length(U) == size(K, 1) == length(F) == length(dirichlet)
-    @assert size(K, 1) == size(K, 2)
-    for i in Iterators.filter(i -> dirichlet[i], eachindex(dirichlet))
-        @inbounds F .-= U[i] * K[:, i]
+function dirichlet_to_neumann!(F::AbstractVector, U::AbstractVector, K::AbstractMatrix, dirichlet::AbstractVector{Bool})
+    @assert length(U) == size(K, 1) == size(K, 2) == length(F) == length(dirichlet)
+    @inbounds for j in Iterators.filter(j -> dirichlet[j], eachindex(dirichlet))
+        for i in 1:length(F)
+            F[i] -= U[j] * K[i, j]
+        end
     end
+end
+function dirichlet_to_neumann!(F::AbstractVector, U::AbstractVector, K::SparseMatrixCSC, dirichlet::AbstractVector{Bool})
+    @assert length(U) == size(K, 1) == size(K, 2) == length(F) == length(dirichlet)
+    rows = rowvals(K)
+    vals = nonzeros(K)
+    @inbounds for j in Iterators.filter(j -> dirichlet[j], eachindex(dirichlet))
+        for i in nzrange(K, j)
+            row = rows[i]
+            val = vals[i]
+            F[row] -= U[row] * val
+        end
+    end
+end
+
+function solve!(U::AbstractVector, K::AbstractMatrix, F::AbstractVector, dirichlet::AbstractVector{Bool})
+    @assert length(U) == size(K, 1) == size(K, 2) == length(F) == length(dirichlet)
+    dirichlet_to_neumann!(F, U, K, dirichlet)
     fdofs = findall(.!dirichlet)
     @inbounds U[fdofs] = K[fdofs, fdofs] \ F[fdofs]
     U
