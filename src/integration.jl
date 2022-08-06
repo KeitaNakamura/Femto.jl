b076d30a5687be5a70a227ef5cc086d74e004f0b
Keita Nakamura <keita.nakamura.1109@gmail.com>
2022-08-07 01:44:47 +0900

    Add kwargs to integrate_matrix and integrate_vector

--- src/integration.jl
+++ src/integration.jl
@@ -1,11 +1,11 @@
-function integrate_matrix(f, args...)
+function integrate_matrix(f, args...; kwargs...)
     T = infer_integrate_matrix_eltype(f, args...)
     A = create_matrix(T, args...)
-    integrate!(f, A, args...)
+    integrate!(f, A, args...; kwargs...)
 end
 
-function integrate_vector(f, args...)
+function integrate_vector(f, args...; kwargs...)
     T = infer_integrate_vector_eltype(f, args...)
     A = create_vector(T, args...)
-    integrate!(f, A, args...)
+    integrate!(f, A, args...; kwargs...)
 end
