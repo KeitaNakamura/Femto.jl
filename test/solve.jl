65c7e0a8ea00f17d8cbb96aa72dfa63aea748095
Keita Nakamura <keita.nakamura.1109@gmail.com>
2022-08-31 22:23:06 +0900

    Add symmetric option to `linsolve!` and `nlsolve!` (#50)
    * Add symmetric option to `linsolve!` and `nlsolve!`
    
    * Add tests

--- test/solve.jl
+++ test/solve.jl
@@ -1,5 +1,7 @@
 @testset "Solve" begin
     Random.seed!(1234)
+
+    ## not symmetric
     K = sprand(50, 50, 0.2)
     F = rand(50)
     dirichlet = rand(Bool, 50)
@@ -10,4 +12,15 @@
     U2 = zeros(50)
     linsolve!(U2, Array(K), F, dirichlet)
     @test U1 ≈ U2
+
+    ## symmetric
+    Us1 = zeros(50)
+    # SparseMatrixCSC
+    linsolve!(U1, K+K', F, dirichlet; symmetric=false)
+    linsolve!(Us1, K+K', F, dirichlet; symmetric=true)
+    # Matrix
+    Us2 = zeros(50)
+    linsolve!(U2, Array(K+K'), F, dirichlet; symmetric=false)
+    linsolve!(Us2, Array(K+K'), F, dirichlet; symmetric=true)
+    @test U1 ≈ U2 ≈ Us1 ≈ Us2
 end
