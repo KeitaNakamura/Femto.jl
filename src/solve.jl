39838877adca38999b7b081df7c9c8867210c8fd
Keita Nakamura <keita.nakamura.1109@gmail.com>
2022-09-08 16:16:24 +0900

    Support `Symmetric` and `Diagonal` in `linsolve`, remove `symmetric` option

--- src/solve.jl
+++ src/solve.jl
@@ -19,24 +19,24 @@ function dirichlet_to_neumann!(F::AbstractVector, U::AbstractVector, K::SparseMa
     end
 end
 
-function linsolve!(U::AbstractVector, K::AbstractMatrix, F::AbstractVector; symmetric::Bool=false)
+function linsolve!(U::AbstractVector, K::AbstractMatrix, F::AbstractVector)
     @assert length(U) == size(K, 1) == size(K, 2) == length(F)
-    if symmetric
-        @inbounds U .= Symmetric(K) \ F
-    else
-        @inbounds U .= K \ F
-    end
+    @inbounds U .= K \ F
     U
 end
 
-function linsolve!(U::AbstractVector, K::AbstractMatrix, F::AbstractVector, dirichlet::AbstractVector{Bool}; symmetric::Bool=false)
+get_K_fdofs(K::AbstractMatrix, fdofs::Vector{Int}) = (@_propagate_inbounds_meta; K[fdofs,fdofs])
+get_K_fdofs(K::Symmetric, fdofs::Vector{Int}) = (@_propagate_inbounds_meta; Symmetric(K.data[fdofs,fdofs], Symbol(K.uplo)))
+get_K_fdofs(K::Diagonal, fdofs::Vector{Int}) = (@_propagate_inbounds_meta; Diagonal(diag(K)[fdofs]))
+
+function linsolve!(U::AbstractVector, K::AbstractMatrix, F::AbstractVector, dirichlet::AbstractVector{Bool})
     @assert length(U) == size(K, 1) == size(K, 2) == length(F) == length(dirichlet)
     dirichlet_to_neumann!(F, U, K, dirichlet)
     fdofs = findall(.!dirichlet)
-    @inbounds linsolve!(view(U, fdofs), K[fdofs, fdofs], F[fdofs]; symmetric)
+    @inbounds linsolve!(view(U, fdofs), get_K_fdofs(K, fdofs), F[fdofs])
     U
 end
-linsolve!(U::AbstractVector, K::SparseMatrixCOO, F::AbstractVector, dirichlet::AbstractVector{Bool}; symmetric::Bool=false) = linsolve!(U, sparse(K), F, dirichlet; symmetric)
+linsolve!(U::AbstractVector, K::SparseMatrixCOO, F::AbstractVector, dirichlet::AbstractVector{Bool}) = linsolve!(U, sparse(K), F, dirichlet)
 
 function nlsolve!(f!, U::AbstractVector{T}, dirichlet::AbstractVector{Bool}, args...; maxiter::Int=20, tol::Real=1e-8, symmetric::Bool=false) where {T <: Real}
     @assert length(U) == length(dirichlet)
@@ -47,7 +47,11 @@ function nlsolve!(f!, U::AbstractVector{T}, dirichlet::AbstractVector{Bool}, arg
     fdofs = findall(.!dirichlet)
     for step in 1:maxiter
         f!(R, J, U, args...)
-        @inbounds linsolve!(view(dU, fdofs), J[fdofs,fdofs], R[fdofs]; symmetric)
+        if symmetric
+            @inbounds linsolve!(view(dU, fdofs), get_K_fdofs(Symmetric(J), fdofs), R[fdofs])
+        else
+            @inbounds linsolve!(view(dU, fdofs), get_K_fdofs(J, fdofs), R[fdofs])
+        end
         @. U -= dU
         norm(dU)<tol && norm(U)<tol && return
         norm(dU)/norm(U) < tol && return
