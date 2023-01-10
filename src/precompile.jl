c9c93ebb302766b0f57ab20d110d6d59b5c64e2b
Keita Nakamura <keita.nakamura.1109@gmail.com>
2023-01-10 10:49:49 +0900

    Modify precompile workload (#76)
    * Modify precompile workload
    
    * Set version to 0.11.3

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,5 +1,8 @@
-function _precompile_()
-    for S in (Line2, Line3, Quad4, Quad9, Hex8, Hex27, Tri3, Tri6, Tet4, Tet10)
-        @assert precompile(Tuple{Type{Femto.Element{T, dim} where dim where T}, S})
+using SnoopPrecompile
+
+@precompile_all_calls begin
+    for shape in (Line2(), Line3(), Quad4(), Quad9(), Hex8(), Hex27(), Tri3(), Tri6(), Tet4(), Tet10())
+        Element(shape)
+        get_dimension(shape) != 3 && FaceElement(shape)
     end
 end
